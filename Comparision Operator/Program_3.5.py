2566ff6428a7841a7a186709df0df689710e5de1
Ved Gabani <ved3443gaming@gmail.com>
2026-04-18 18:08:48 +0530

    Add files via upload

---
+++ Comparision Operator/Program_3.5.py
@@ -0,0 +1,11 @@
+# Check Pass or Fail
+
+print("----- Pass or Fail -----\n")
+
+a = int(input("Enter your Marks -_- "))
+
+if a>=35:
+    print("You are Pass.....")
+
+else:
+    print("You are Fail....")
