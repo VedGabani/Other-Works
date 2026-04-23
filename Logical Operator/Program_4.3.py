21d181293c25ff99cafd0a2d841c80dc22ccb2d5
Ved Gabani <ved3443gaming@gmail.com>
2026-04-23 10:35:49 +0530

    Update Program_4.3.py

--- Logical Operator/Program_4.3.py
+++ Logical Operator/Program_4.3.py
@@ -2,7 +2,7 @@
 
 print("----- Leap Year -----\n")
 
-year = int(input("Enter a year: "))
+year = int(input("Enter a year -_- "))
 
 if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
     print("Leap Year ✅")
