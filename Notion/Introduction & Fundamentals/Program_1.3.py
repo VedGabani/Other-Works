c0a5bc9ae2405f7ce9ed45fc5b1089d91d1cc840
Ved Gabani <ved3443gaming@gmail.com>
2026-05-14 11:13:40 +0530

    Add files via upload

---
+++ Notion/Introduction & Fundamentals/Program_1.3.py
@@ -0,0 +1,11 @@
+# Q-3
+
+a = int(input("Enter a number -_- "))
+b = float(input("Enter number -_- "))
+c = input("Enter a word -_- ")
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
