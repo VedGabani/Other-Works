4d5fb6f8f1a7ef435d618ccdf97f5bfbcf66748d
Ved Gabani <ved3443gaming@gmail.com>
2026-05-14 11:11:07 +0530

    Add files via upload

---
+++ Program_1.3.py
@@ -0,0 +1,9 @@
+a = int(input("Enter a number -_- "))
+b = float(input("Enter number -_- "))
+c = input("Enter a word -_- ")
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
