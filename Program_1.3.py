20e934db07015978a2591312aacf916f1baf128e
Ved Gabani <ved3443gaming@gmail.com>
2026-05-15 10:56:03 +0530

    Delete Program_1.3.py

--- Program_1.3.py
+++
@@ -1,9 +0,0 @@
-a = int(input("Enter a number -_- "))
-b = float(input("Enter number -_- "))
-c = input("Enter a word -_- ")
-d = True
-
-print(type(a))
-print(type(b))
-print(type(c))
-print(type(d))
